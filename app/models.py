a87f888b2c891dfb03512a0a29291e801e2cd256
Nori Kaneshige <39219420+NoriKaneshige@users.noreply.github.com>
2020-05-06 11:32:59 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField('タイトル', max_length=255)
+    title = models.CharField('title', max_length=255)
 
     def __str__(self):
         return self.title
